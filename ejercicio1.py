580cf8c3818383a3444c3b44017b21355e042568
nicomn97-examenfinal_metodos-dhcjz3sp <nicomn97-examenfinal_metodos-dhcjz3sp@local>
2018-05-25 00:37:19 +0000

    commit final del punto 2 y grafica

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,9 +4,24 @@
 # Guarde esta segunda derivada en funcion de x en una grafica llamada 'segunda.png'
 
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 x = np.linspace(0,2.,10)
 fx = np.array([0., 0.0494, 0.1975, 0.4444, 0.7901,1.2346 , 1.7778, 2.4198, 3.1605, 4.])
+
+dx=x[1]-x[0]
+d=(fx[2:]-fx[:-2])/(dx*dx)
+
+plt.figure()
+plt.scatter(d,x[1:-1], label="derivada")
+plt.xlabel("x")
+plt.ylabel("derivada")
+plt.title("Derivada de f(x)")
+plt.legend()
+plt.savefig("segunda.png")
+
 
 
 
