409055fd43a8312f05ac2e121711dd8857980512
nicomn97-examenfinal_metodos-dhcjz3sp <nicomn97-examenfinal_metodos-dhcjz3sp@local>
2018-05-25 00:35:30 +0000

    commit final del punto 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -7,4 +7,16 @@ x = np.int_(np.random.random(100)*1000)
 print(x)
 
 
+i=0
+n=np.shape(x)[0]
+
+while (i<n):
+    if(x[i]>800):
+        i=n
+    elif(x[i]%2!=0):
+        print (x[i])
+    i=i+1
+        
+
+
 
